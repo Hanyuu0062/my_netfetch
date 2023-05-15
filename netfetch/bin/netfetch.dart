736b378e773f0d5999573e662cc21e86cd3d3992
Hanyuu 1 <1183580621@qq.com>
2023-05-15 01:48:52 +0000

    finished

--- netfetch/bin/netfetch.dart
+++ netfetch/bin/netfetch.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart' as http;
 
 void main() async {
-  final httpPackageUrl = Uri.https('dart.dev', '/f/packages/http.json');
+  final httpPackageUrl = Uri.https(
+      'raw.githubusercontent.com', '/Hanyuu0062/my_netfetch/master/test.json');
   final httpPackageInfo = await http.read(httpPackageUrl);
   print(httpPackageInfo);
 }
